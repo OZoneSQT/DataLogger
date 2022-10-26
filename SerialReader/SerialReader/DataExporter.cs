759f69fb18511529f8d71c64cc57b2b867e508a5
Michel Sommer <skruen@gmail.com>
2022-10-27 00:49:55 +0200

    Windows application added
    Warning: Needs hardware for testing
    before compiling to msi/exe file

---
+++ SerialReader/SerialReader/DataExporter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace SerialReader
+{
+    internal class DataExporter
+    {
+        private string fileName;
+
+        public DataExporter()
+        {
+            fileName = DateBuilder();
+            Console.WriteLine("Data will be stored here: " + fileName);
+        }
+
+        private String DateBuilder()
+        {
+            DateTime timestamp = DateTime.Now;
+            String year = Convert.ToString(timestamp.Year);
+            String month = Convert.ToString(timestamp.Month);
+            String day = Convert.ToString(timestamp.Day);
+            String hh = Convert.ToString(timestamp.Hour);
+            String mm = Convert.ToString(timestamp.Minute);
+            String ss = Convert.ToString(timestamp.Second);
+            return fileName = "C:\\SerialReader_" + year + month + day + hh + mm + ss + ".csv";
+        }
+
+        public void WriteToFile(String datapacket)
+        {
+            if (!File.Exists(fileName))
+            {
+                string clientHeader = "Logged data:" + Environment.NewLine;
+
+                File.WriteAllText(fileName, clientHeader);
+            }
+
+            File.AppendAllText(fileName, datapacket);
+            Console.WriteLine(datapacket);
+        }
+
+    }
+}
---
+++ SerialReader/SerialReader/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace SerialReader
+{
+    internal class Program
+    {
+        private static int baud = 9600;                    
+        private static Parity parity = Parity.None;
+        private static int packetSize = 8;                  // number of bits in a data packet OBS is this right???
+        private static StopBits stopBits = StopBits.One;    // is this right???
+
+        public static int Main(string[] args)
+        {
+            int i = 0;
+
+            Console.WriteLine("Welcome to SerialReader...");
+            Console.WriteLine("Save Serial data stream to *.csv file");
+
+            try
+            {
+                Console.WriteLine("\nStartup...");
+
+                SerialCom serialCom = new SerialCom();
+                serialCom.SetSerialPort(baud, parity, packetSize, stopBits);
+
+
+                i = serialCom.MainAsync(args).GetAwaiter().GetResult();
+
+
+                serialCom.EndConnection();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The startup process exits with error: " + e.StackTrace);
+                Console.WriteLine("\nTry to run program as admin (Right - clik on program and select[Run as administrator]).");
+
+                System.Threading.Thread.Sleep(10000);
+
+                Environment.Exit(1);
+            }
+
+            return i;
+        }
+
+    }
+}
